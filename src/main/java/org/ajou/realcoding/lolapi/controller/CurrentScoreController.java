c23c036f4f872846d9929fe111e1519aa619449e
Jieun <yjeka@ajou.ac.kr>
2020-07-17 12:29:57 +0900

    Add getQueueType method

--- src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
+++ src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
@@ -1,20 +1,25 @@
 package org.ajou.realcoding.lolapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.lolapi.domain.SoloRankInfo;
 import org.ajou.realcoding.lolapi.service.CurrentScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class CurrentScoreController {
 
     @Autowired
     private CurrentScoreService currentScoreService;
 
-    @GetMapping("")
-    public UserInfo getUserID(@RequestParam String summonerName) {
-        return currentScoreService.getUserID(summonerName);
+    @GetMapping("/LOL/queueType/RANKED_SOLO_5*5")
+    public SoloRankInfo getQueueType(@RequestParam String summonerName) {
+        return currentScoreService.getQueueType(summonerName);
     }
+
 
 }
