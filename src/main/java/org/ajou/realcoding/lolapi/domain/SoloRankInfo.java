55a4dcd3f858a67f1686a92a9e2cecbc26fef466
chan <baewonchan1@naver.com>
2020-07-17 12:33:44 +0900

    Modify domain attribute type

---
+++ src/main/java/org/ajou/realcoding/lolapi/domain/SoloRankInfo.java
@@ -0,0 +1,22 @@
+package org.ajou.realcoding.lolapi.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class SoloRankInfo {
+    @Id
+    private String leagueId;
+    private String queueType;
+    private String tier;
+    private String rank;
+    private String summonerId;
+    private String summonerName;
+    private int leaguePoints;
+    private int wins;
+    private int losses;
+    private boolean veteran;
+    private boolean inactive;
+    private boolean freshBlood;
+    private boolean hotStreak;
+}
