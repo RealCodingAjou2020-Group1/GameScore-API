4fee9ee410fcc3cf29adc8b4d03d96db4f76fe1c
Jieun <yjeka@ajou.ac.kr>
2020-07-17 12:35:58 +0900

    Add step3 service

--- src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
+++ src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
@@ -1,4 +1,23 @@
 package org.ajou.realcoding.lolapi.service;
 
+import org.ajou.realcoding.lolapi.api.ScoreOpenApiClient;
+import org.ajou.realcoding.lolapi.domain.SoloRankInfo;
+import org.ajou.realcoding.lolapi.repository.CurrentScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class CurrentScoreService {
+    @Autowired
+    private ScoreOpenApiClient scoreOpenApiClient;
+    @Autowired
+    private CurrentScoreRepository currentScoreRepository;
+
+    public SoloRankInfo getQueueType(String encryptedSummonerId) {
+        SoloRankInfo soloRankInfo = scoreOpenApiClient.getQueueType(encryptedSummonerId);
+        currentScoreRepository.saveCurrentSummonerName(soloRankInfo);
+        return currentScoreRepository.findUserInfoByName(encryptedSummonerId);
+    }
 }
