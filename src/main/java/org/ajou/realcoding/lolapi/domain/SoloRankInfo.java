a49b18c5cd08e907164256f5cc1fc83acf08b90f
chan <baewonchan1@naver.com>
2020-07-17 14:06:46 +0900

    Remove conflict in SoloRankInfo domain

--- src/main/java/org/ajou/realcoding/lolapi/domain/SoloRankInfo.java
+++ src/main/java/org/ajou/realcoding/lolapi/domain/SoloRankInfo.java
@@ -1,17 +1,11 @@
 package org.ajou.realcoding.lolapi.domain;
 
 import lombok.Data;
-<<<<<<<<< Temporary merge branch 1
-
-@Data
-public class SoloRankInfo {
-=========
 import org.springframework.data.annotation.Id;
 
 @Data
 public class SoloRankInfo {
     @Id
->>>>>>>>> Temporary merge branch 2
     private String leagueId;
     private String queueType;
     private String tier;
@@ -21,11 +15,8 @@ public class SoloRankInfo {
     private int leaguePoints;
     private int wins;
     private int losses;
-<<<<<<<<< Temporary merge branch 1
-=========
     private boolean veteran;
     private boolean inactive;
     private boolean freshBlood;
     private boolean hotStreak;
->>>>>>>>> Temporary merge branch 2
 }
