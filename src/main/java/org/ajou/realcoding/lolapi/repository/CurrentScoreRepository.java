dfa16ecfb1a01dc48368fb629ddf78039af9e481
Jieun <yjeka@ajou.ac.kr>
2020-07-17 16:02:49 +0900

    Add step3 repository

--- src/main/java/org/ajou/realcoding/lolapi/repository/CurrentScoreRepository.java
+++ src/main/java/org/ajou/realcoding/lolapi/repository/CurrentScoreRepository.java
@@ -1,4 +1,32 @@
 package org.ajou.realcoding.lolapi.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.lolapi.domain.SoloRankInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Slf4j
 public class CurrentScoreRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void saveCurrentSummonerName(SoloRankInfo soloRankInfo){
+        SoloRankInfo saveQueueType = mongoTemplate.save(soloRankInfo);
+        log.info("Saved: {}", saveQueueType);
+    }
+
+
+    public SoloRankInfo findUserInfoByName(String encryptedSummonerId) {
+        Query query = Query.query(
+                Criteria.where("summonerId").regex(encryptedSummonerId, "i")
+        );
+        SoloRankInfo soloRankInfo = mongoTemplate.findOne(query, SoloRankInfo.class);
+        log.info("USER INFO: {}", soloRankInfo.toString());
+        return soloRankInfo;
+    }
 }
