c7076a7c0c2fd9e3f3655d1d725aa0cf29652297
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-18 23:09:57 +0900

    Add method getGameId in CurrentScoreService.java

--- src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
+++ src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
@@ -1,4 +1,34 @@
 package org.ajou.realcoding.lolapi.service;
 
-public class CurrentScoreService {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ajou.realcoding.lolapi.api.ScoreOpenApiClient;
+import org.ajou.realcoding.lolapi.domain.MatchInfo;
+import org.ajou.realcoding.lolapi.repository.CurrentScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@Service
+@EnableScheduling
+public class CurrentScoreService
+{
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ScoreOpenApiClient scoreOpenApiClient;
+
+    private Queue<Long> gameIdQueue = new LinkedList<>();
+    @Autowired
+    private CurrentScoreRepository currentScoreRepository;
+
+    @GetMapping("/lol/match/v4/matchlists/by-account")
+    public MatchInfo getGameId(String accountId)
+    {
+        return currentScoreRepository.findGameId(accountId);
+    }
+
 }
