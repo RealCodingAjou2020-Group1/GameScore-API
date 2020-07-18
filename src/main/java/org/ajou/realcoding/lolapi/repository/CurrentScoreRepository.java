cdec7de7bb696606f5fd3335af886b4f16fd19f2
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-18 23:17:20 +0900

    Add method findGameId in CurrentScoreRepository.java

--- src/main/java/org/ajou/realcoding/lolapi/repository/CurrentScoreRepository.java
+++ src/main/java/org/ajou/realcoding/lolapi/repository/CurrentScoreRepository.java
@@ -1,4 +1,25 @@
 package org.ajou.realcoding.lolapi.repository;
 
-public class CurrentScoreRepository {
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.lolapi.domain.MatchInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class CurrentScoreRepository
+{
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public MatchInfo findGameId(String accountId)
+    {
+        Query query = Query.query(Criteria.where("_id").is(accountId));
+
+        MatchInfo matchInfo = mongoTemplate.find(query, MatchInfo.class);
+        return matchInfo;
+    }
 }
