b1df27527a6601dbef5475d9a28f378a790d51c8
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-18 23:31:55 +0900

    Fix method getCurrentGameIdEveryFiveSeconds in CurrentScoreService.java

--- src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
+++ src/main/java/org/ajou/realcoding/lolapi/service/CurrentScoreService.java
@@ -3,12 +3,15 @@ package org.ajou.realcoding.lolapi.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ajou.realcoding.lolapi.api.ScoreOpenApiClient;
 import org.ajou.realcoding.lolapi.domain.MatchInfo;
+import org.ajou.realcoding.lolapi.domain.UserInfo;
 import org.ajou.realcoding.lolapi.repository.CurrentScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -21,6 +24,8 @@ public class CurrentScoreService
     @Autowired
     private ScoreOpenApiClient scoreOpenApiClient;
 
+    private String accountId1;
+
     private Queue<Long> gameIdQueue = new LinkedList<>();
     @Autowired
     private CurrentScoreRepository currentScoreRepository;
@@ -28,7 +33,15 @@ public class CurrentScoreService
     @GetMapping("/lol/match/v4/matchlists/by-account")
     public MatchInfo getGameId(String accountId)
     {
+        accountId1 = accountId;
         return currentScoreRepository.findGameId(accountId);
+    }
+
+    @Scheduled(fixedDelay = 5000L)
+    public void getCurrentGameIdEveryFiveSeconds()
+    {
+        UserInfo userInfo = scoreOpenApiClient.getGameId(accountId1);
+        currentScoreRepository.saveGameId(userInfo);
     }
 
 }
