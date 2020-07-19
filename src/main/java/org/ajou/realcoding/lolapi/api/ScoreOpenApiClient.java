fcf222f40a0b4709252d0cc35ab9df479d836714
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-19 19:13:51 +0900

    Fix return type of method getGameId in ScoreOpenApiClient.java

--- src/main/java/org/ajou/realcoding/lolapi/api/ScoreOpenApiClient.java
+++ src/main/java/org/ajou/realcoding/lolapi/api/ScoreOpenApiClient.java
@@ -1,23 +1,20 @@
 package org.ajou.realcoding.lolapi.api;
 
-import org.ajou.realcoding.lolapi.domain.MatchInfo;
-import org.ajou.realcoding.lolapi.domain.UserInfo;
+import org.ajou.realcoding.lolapi.domain.GameIds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import javax.jws.soap.SOAPBinding;
 
 @Service
 public class ScoreOpenApiClient
 {
     @Autowired
     private RestTemplate restTemplate;
-    private static final String GAMEID_REQUEST_URI = "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/{accountId}?api_key=RGAPI-b746212d-68ce-4165-aa9d-bcffe96c00a4";
+    private static final String GAMEID_REQUEST_URI = "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/{accountId}?api_key=RGAPI-f6535e71-2ccb-45db-8d3b-38cf9cf90902";
 
-    public MatchInfo getGameId(String accountId)
+    public GameIds getGameId(String accountId)
     {
-        MatchInfo matchInfo = restTemplate.getForObject(GAMEID_REQUEST_URI, MatchInfo.class, accountId);
-        return matchInfo;
+        GameIds gameIds = restTemplate.getForObject(GAMEID_REQUEST_URI, GameIds.class, accountId);
+        return gameIds;
     }
 }
