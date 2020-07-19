fc3ef7de69cc387c152d653b31c8cc005b06c6d8
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-19 19:12:29 +0900

    Add startIndex, totalGames, and endIndex in GameIds.java

--- src/main/java/org/ajou/realcoding/lolapi/domain/GameIds.java
+++ src/main/java/org/ajou/realcoding/lolapi/domain/GameIds.java
@@ -1,14 +1,20 @@
 package org.ajou.realcoding.lolapi.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Data
 public class GameIds
 {
-    List<MatchReferenceDto> matches;
+    @Id
+    private int startIndex;
+    private int totalGames;
+    private int endIndex;
+    private List<MatchReferenceDto> matches;
 
+    @Data
     public static class MatchReferenceDto
     {
         private long gameId;
