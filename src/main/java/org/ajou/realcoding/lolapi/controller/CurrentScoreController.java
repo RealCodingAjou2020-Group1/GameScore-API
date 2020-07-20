37c27484f531eb684cecd1b26adeed6a813944e1
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-20 23:13:07 +0900

    Replace method getGameId to method getAnalysis in CurrentScoreController.java

--- src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
+++ src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
@@ -1,21 +1,31 @@
 package org.ajou.realcoding.lolapi.controller;
 
-import org.ajou.realcoding.lolapi.domain.GameIds;
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.lolapi.domain.Analysis;
+import org.ajou.realcoding.lolapi.domain.SoloRankInfo;
 import org.ajou.realcoding.lolapi.service.CurrentScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
-public class CurrentScoreController
-{
+public class CurrentScoreController {
+
     @Autowired
     private CurrentScoreService currentScoreService;
 
-    @GetMapping("/lol/match/v4/matchlists/by-account")
-    public GameIds getGameId(@RequestParam String accountId)
-    {
-        return currentScoreService.getGameId(accountId);
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
+    public List<Analysis> getAnalysis(@RequestParam String summonerName) throws ParseException {
+        currentScoreService.getSoloRankInfo(currentScoreService.getUserInfo(summonerName).getId());
+        return currentScoreService.getAnalysis(currentScoreService.getFiveGameId(currentScoreService.getGameId(currentScoreService.getUserInfo(summonerName).getAccountId())), summonerName);
     }
+
 }
