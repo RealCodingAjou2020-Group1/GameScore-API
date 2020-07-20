c9758019e0b7fa101f049f9840e201c8f5c2f332
SYJoe <whtjddus8631@ajou.ac.kr>
2020-07-20 23:07:33 +0900

    Fix class UserInfo.java in package domain

--- src/main/java/org/ajou/realcoding/lolapi/domain/UserInfo.java
+++ src/main/java/org/ajou/realcoding/lolapi/domain/UserInfo.java
@@ -1,11 +1,13 @@
 package org.ajou.realcoding.lolapi.domain;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+@Data
 public class UserInfo {
     @Id
     private String id;
     private String accountId;
-    private String puuId;
+    //private String puuId;
     private String name;
 }
