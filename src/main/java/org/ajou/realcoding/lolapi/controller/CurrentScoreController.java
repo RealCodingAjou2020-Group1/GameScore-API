fdd5b5d94c64d06d0e761cddf74616f8ffb7bfa5
chan <baewonchan1@naver.com>
2020-07-20 23:49:11 +0900

    Modify controller name and return type

--- src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
+++ src/main/java/org/ajou/realcoding/lolapi/controller/CurrentScoreController.java
@@ -2,7 +2,9 @@ package org.ajou.realcoding.lolapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.lolapi.domain.Analysis;
+import org.ajou.realcoding.lolapi.domain.Result;
 import org.ajou.realcoding.lolapi.domain.SoloRankInfo;
+import org.ajou.realcoding.lolapi.domain.UserInfo;
 import org.ajou.realcoding.lolapi.service.CurrentScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +25,9 @@ public class CurrentScoreController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
-    public List<Analysis> getAnalysis(@RequestParam String summonerName) {
-         currentScoreService.getSoloRankInfo(currentScoreService.getUserInfo(summonerName).getId());
-        return currentScoreService.getAnalysis(currentScoreService.getFiveGameId(currentScoreService.getGameId(currentScoreService.getUserInfo(summonerName).getAccountId())), summonerName);
+    public Result getResult(@RequestParam String summonerName) throws ParseException {
+        UserInfo userInfo = currentScoreService.getUserInfo(summonerName);
+        currentScoreService.getSoloRankInfo(userInfo.getId());
+        return currentScoreService.getAnalysis(currentScoreService.getFiveGameId(currentScoreService.getGameId(userInfo.getAccountId())), userInfo.getAccountId(), summonerName);
     }
-
 }
